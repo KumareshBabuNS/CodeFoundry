33d6c34afee0bcfd8d6ea46b5e15dae41bb936b3
Vuk Petrovic <vukpetrovic1@yahoo.com>
2016-01-23 13:16:13 -0500

    Finished header, working on apiPost directive

--- src/js/AppModule.js
+++ src/js/AppModule.js
@@ -4,6 +4,10 @@ var app = angular.module("CodeFoundry", ['ngRoute', 'ngMaterial', 'ngAnimate']);
 app.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider
+      .when('/posts', {
+      	templateUrl: 'templates/posts.html',
+        controller: 'postsController'
+      })
       .when('/', {
       	templateUrl: 'templates/home.html',
         controller: 'homeController'
@@ -13,6 +17,54 @@ app.config(['$routeProvider',
       });
 }]);
 
+//defines application-wide constants
+app.run(function($rootScope) {
+	$rootScope.logoIcon = "assets/anvil.png";
+	$rootScope.appName = "CodeFoundry";
+	$rootScope.settingsOpen = false;
+});
+
 app.controller('homeController', function($scope, $http) {
-	$scope.test = "Hello, World";
+	$scope.msg = "This is the home page, under construction.";
+});
+
+app.controller('postsController', function($scope, $http, $rootScope) {
+	$rootScope.profileOptions = [
+		"Profile",
+		"Your Likes",
+		"Log Out"
+	]
+	$rootScope.langOptions = [
+		"Any Language",
+		"JavaScript",
+		"Java",
+		"C",
+		"C++",
+		"Python",
+		"Ruby",
+		"PHP",
+		"HTML",
+		"CSS",
+		"C#",
+		"Objective-C",
+		"Swift",
+		"GoLang",
+		"TCL",
+		"R",
+		"D"
+	];
+
+	$rootScope.typeOptions = [
+		"Any",
+		"Free",
+		"Premium"
+	];
+
+	//default search settings 
+	$rootScope.searchQuery = {
+		searchString: "",
+		searchType: "Any",
+		searchLanguage: "JavaScript"
+	}
+
 });
